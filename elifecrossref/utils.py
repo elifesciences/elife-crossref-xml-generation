11d3c3ce6584c8e615f3c5a4d5c77f3fe4637c3d
Graham Nott <gnott@starglobal.ca>
2017-06-28 17:31:33 -0700

    Generate crossref deposits using elifearticle and existing test scenarios.

---
+++ elifecrossref/utils.py
@@ -0,0 +1,77 @@
+from xml.etree.ElementTree import SubElement
+from git import Repo
+
+def get_last_commit_to_master():
+    """
+    returns the last commit on the master branch. It would be more ideal to get the commit
+    from the branch we are currently on, but as this is a check mostly to help
+    with production issues, returning the commit from master will be sufficient.
+    """
+    repo = Repo(".")
+    last_commit = None
+    try:
+        last_commit = repo.commits()[0]
+    except AttributeError:
+        # Optimised for version 0.3.2.RC1
+        last_commit = repo.head.commit
+    return str(last_commit)
+    # commit =  repo.heads[0].commit
+    # return str(commit)
+
+def append_minidom_xml_to_elementtree_xml(parent, xml, recursive=False, attributes=None):
+    """
+    Recursively,
+    Given an ElementTree.Element as parent, and a minidom instance as xml,
+    append the tags and content from xml to parent
+    Used primarily for adding a snippet of XML with <italic> tags
+    attributes: a list of attribute names to copy
+    """
+
+    # Get the root tag name
+    if recursive is False:
+        tag_name = xml.documentElement.tagName
+        node = xml.getElementsByTagName(tag_name)[0]
+        new_elem = SubElement(parent, tag_name)
+        if attributes:
+            for attribute in attributes:
+                if xml.documentElement.getAttribute(attribute):
+                    new_elem.set(attribute, xml.documentElement.getAttribute(attribute))
+    else:
+        node = xml
+        tag_name = node.tagName
+        new_elem = parent
+
+    i = 0
+    for child_node in node.childNodes:
+        if child_node.nodeName == '#text':
+            if not new_elem.text and i <= 0:
+                new_elem.text = child_node.nodeValue
+            elif not new_elem.text and i > 0:
+                new_elem_sub.tail = child_node.nodeValue
+            else:
+                new_elem_sub.tail = child_node.nodeValue
+
+        elif child_node.childNodes is not None:
+            new_elem_sub = SubElement(new_elem, child_node.tagName)
+            new_elem_sub = append_minidom_xml_to_elementtree_xml(new_elem_sub, child_node,
+                                                                 True, attributes)
+
+        i = i + 1
+
+    # Debug
+    #encoding = 'utf-8'
+    #rough_string = ElementTree.tostring(parent, encoding)
+    #print rough_string
+
+    return parent
+
+def elife_journal_volume(pub_date):
+    """
+    volume value is based on the pub date year
+    pub_date is a python time object
+    """
+    try:
+        volume = str(pub_date.tm_year - 2011)
+    except:
+        volume = None
+    return volume
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,14 +1,59 @@
 import unittest
 import time
+import os
+import re
 from elifecrossref import generate
+from elifearticle import parse
+
+TEST_BASE_PATH = os.path.dirname(os.path.abspath(__file__)) + os.sep
+TEST_DATA_PATH = TEST_BASE_PATH + "test_data" + os.sep
+generate.TMP_DIR = TEST_BASE_PATH + "tmp" + os.sep
 
 class TestGenerate(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.passes = []
+        self.passes.append(('elife-02935-v2.xml', 'elife-crossref-02935-20160728134203.xml'))
+        self.passes.append(('elife_poa_e02725.xml', 'elife-crossref-02725-20160513174001.xml'))
+        self.passes.append(('elife-15743-v1.xml', 'elife-crossref-15743-20160513133358.xml'))
+        self.passes.append(('elife-02020-v1.xml', 'elife-crossref-02020-20160728134532.xml'))
+        self.passes.append(('elife-08206-v3.xml', 'elife-crossref-08206-20160728134951.xml'))
+        self.passes.append(('elife-04637-v2.xml', 'elife-crossref-04637-20160513134549.xml'))
+        self.passes.append(('elife-02043-v2.xml', 'elife-crossref-02043-20160728135127.xml'))
+        self.passes.append(('elife-16988-v1.xml', 'elife-crossref-16988-20160728133801.xml'))
+        self.passes.append(('elife-12444-v2.xml', 'elife-crossref-12444-20160729011053.xml'))
+        self.passes.append(('elife-00666.xml', 'elife-crossref-00666-20170320102118.xml'))
 
-    def test_dummy(self):
-        self.assertEqual(generate.dummy(), None)
+    def clean_crossref_xml_for_comparison(self, xml_content):
+        # For now running a test on a PoA article ignore the
+        # <publication_date media_type="online"> which is set to the date it is generated
+        if '<doi_batch_id>elife-crossref-02725' in xml_content:
+            xml_content = re.sub(ur'<publication_date media_type="online">.*</publication_date>',
+                                 '', xml_content)
+
+        xml_content = re.sub(ur'<!--.*-->', '', xml_content)
+        xml_content = re.sub(ur'<doi_batch_id>.*</doi_batch_id>', '', xml_content)
+        xml_content = re.sub(ur'<timestamp>.*</timestamp>', '', xml_content)
+        return xml_content
+
+    def read_file_content(self, file_name):
+        fp = open(file_name, 'rb')
+        content = fp.read()
+        fp.close()
+        return content
+
+    def test_parse(self):
+        for (article_xml_file, crossref_xml_file) in self.passes:
+            file_path = TEST_DATA_PATH + article_xml_file
+            articles = parse.build_articles_from_article_xmls([file_path])
+            crossref_xml = generate.build_crossref_xml_for_articles(articles)
+
+            model_crossref_xml = self.read_file_content(TEST_DATA_PATH + crossref_xml_file)
+
+            clean_generated_crossref_xml = self.clean_crossref_xml_for_comparison(crossref_xml)
+            clean_model_crossref_xml = self.clean_crossref_xml_for_comparison(model_crossref_xml)
+
+            self.assertEqual(clean_generated_crossref_xml, clean_model_crossref_xml)
 
 
 if __name__ == '__main__':
