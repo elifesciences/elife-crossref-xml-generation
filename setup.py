01e8950bc557d4b38b56771acb4b21afe4994c76
Graham Nott <gnott@starglobal.ca>
2017-07-19 16:58:37 -0700

    Try simplifying the install_requires and load a specific git commit.

--- setup.py
+++ setup.py
@@ -5,16 +5,12 @@ import elifecrossref
 with open('README.rst') as fp:
     readme = fp.read()
 
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
 setup(name='elifecrossref',
     version=elifecrossref.__version__,
     description='eLife Crossref deposit of journal articles.',
     long_description=readme,
     packages=['elifecrossref'],
     license = 'MIT',
-    install_requires=install_requires,
     url='https://github.com/elifesciences/elife-crossref-xml-generation',
     maintainer='eLife Sciences Publications Ltd.',
     maintainer_email='py@elifesciences.org',
