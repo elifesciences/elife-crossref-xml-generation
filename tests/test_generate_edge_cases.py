89317ff468aff640049c807a0874684653e34c91
Graham Nott <gnott@starglobal.ca>
2018-01-17 14:25:57 -0800

    Refactor checking if an article has a license, and tests for do_set_collection.

--- tests/test_generate_edge_cases.py
+++ tests/test_generate_edge_cases.py
@@ -2,7 +2,7 @@ import unittest
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 
-from elifearticle.article import Article, Component, Citation, Dataset, Contributor, Affiliation
+from elifearticle.article import Article, Component, Citation, Dataset, Contributor, Affiliation, License
 
 from elifecrossref import generate
 from elifecrossref.conf import config, parse_raw_config
@@ -388,6 +388,60 @@ class TestGenerateTitles(unittest.TestCase):
         self.assertTrue(expected_contains in crossref_xml_string)
         # now set the config back to normal
         raw_config['face_markup'] = face_markup
+
+
+class TestSetCollection(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def create_aricle_object(self):
+        "create a basic article object"
+        doi = "10.7554/eLife.00666"
+        title = "Test article"
+        article = Article(doi, title)
+        return article
+
+    def create_crossref_object(self, article):
+        "utility to create the crossref object"
+        raw_config = config['elife']
+        crossref_config = parse_raw_config(raw_config)
+        crossref_object = generate.CrossrefXML([article], crossref_config, None, True)
+        return crossref_object
+
+    def test_do_set_collection_no_license(self):
+        "test when an article has no license"
+        # create CrossrefXML object to test
+        article = self.create_aricle_object()
+        crossref_object = self.create_crossref_object(article)
+        # test assertion
+        collection_property = "text-mining"
+        self.assertFalse(crossref_object.do_set_collection(article, collection_property))
+
+    def test_do_set_collection_empty_license(self):
+        "test when an article has no license"
+        article = self.create_aricle_object()
+        # create an empty license
+        license_object = License()
+        article.license = license_object
+        # create CrossrefXML object to test
+        crossref_object = self.create_crossref_object(article)
+        # test assertion
+        collection_property = "text-mining"
+        self.assertFalse(crossref_object.do_set_collection(article, collection_property))
+
+    def test_do_set_collection_with_license(self):
+        "test when an article has no license"
+        article = self.create_aricle_object()
+        # create a license with a href value
+        license_object = License()
+        license_object.href = "http://example.org/license.txt"
+        article.license = license_object
+        # create CrossrefXML object to test
+        crossref_object = self.create_crossref_object(article)
+        # test assertion
+        collection_property = "text-mining"
+        self.assertTrue(crossref_object.do_set_collection(article, collection_property))
 
 
 class TestAddCleanTag(unittest.TestCase):
