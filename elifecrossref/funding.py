18ebbf7e0484a35ad71faaaa8bcfa9ed3bc5b4d5
Graham Nott <gnott@starglobal.ca>
2019-03-15 15:22:23 -0700

    Refactor funding module.

--- elifecrossref/funding.py
+++ elifecrossref/funding.py
@@ -8,21 +8,37 @@ def set_fundref(parent, poa_article):
     if poa_article.funding_awards:
         fr_program_tag = SubElement(parent, 'fr:program')
         fr_program_tag.set("name", "fundref")
-        for award in poa_article.funding_awards:
-            fr_fundgroup_tag = SubElement(fr_program_tag, 'fr:assertion')
-            fr_fundgroup_tag.set("name", "fundgroup")
+    for award in poa_article.funding_awards:
+        set_funding_award(fr_program_tag, award)
 
-            if award.get_funder_name():
-                fr_funder_name_tag = SubElement(fr_fundgroup_tag, 'fr:assertion')
-                fr_funder_name_tag.set("name", "funder_name")
-                fr_funder_name_tag.text = award.get_funder_name()
 
-            if award.get_funder_name() and award.institution_id:
-                fr_funder_identifier_tag = SubElement(fr_funder_name_tag, 'fr:assertion')
-                fr_funder_identifier_tag.set("name", "funder_identifier")
-                fr_funder_identifier_tag.text = award.institution_id
+def set_funding_award(parent, award):
+    fr_fundgroup_tag = SubElement(parent, 'fr:assertion')
+    fr_fundgroup_tag.set("name", "fundgroup")
+    fr_funder_name_tag = set_funder_name(fr_fundgroup_tag, award)
+    if fr_funder_name_tag is not None:
+        set_funder_identifier(fr_funder_name_tag, award)
+    set_award_number(fr_fundgroup_tag, award)
 
-            for award_id in award.award_ids:
-                fr_award_number_tag = SubElement(fr_fundgroup_tag, 'fr:assertion')
-                fr_award_number_tag.set("name", "award_number")
-                fr_award_number_tag.text = award_id
+
+def set_funder_name(parent, award):
+    fr_funder_name_tag = None
+    if award.get_funder_name():
+        fr_funder_name_tag = SubElement(parent, 'fr:assertion')
+        fr_funder_name_tag.set("name", "funder_name")
+        fr_funder_name_tag.text = award.get_funder_name()
+    return fr_funder_name_tag
+
+
+def set_funder_identifier(parent, award):
+    if award.get_funder_name() and award.institution_id:
+        fr_funder_identifier_tag = SubElement(parent, 'fr:assertion')
+        fr_funder_identifier_tag.set("name", "funder_identifier")
+        fr_funder_identifier_tag.text = award.institution_id
+
+
+def set_award_number(parent, award):
+    for award_id in award.award_ids:
+        fr_award_number_tag = SubElement(parent, 'fr:assertion')
+        fr_award_number_tag.set("name", "award_number")
+        fr_award_number_tag.text = award_id
