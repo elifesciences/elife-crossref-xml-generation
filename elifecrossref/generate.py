026f4f856ef86a4ba347ae0c6ee6dd43d02e7c32
Graham Nott <gnott@starglobal.ca>
2019-03-14 18:01:12 -0700

    Refactor mime_types to a module.

--- elifecrossref/generate.py
+++ elifecrossref/generate.py
@@ -12,6 +12,7 @@ from elifetools import xmlio
 
 from elifecrossref import utils
 from elifecrossref.conf import raw_config, parse_raw_config
+from elifecrossref.mime_type import crossref_mime_type
 
 
 TMP_DIR = 'tmp'
@@ -824,9 +825,9 @@ class CrossrefXML(object):
 
             if comp.mime_type:
                 # Convert to allowed mime types for Crossref, if found
-                if self.crossref_mime_type(comp.mime_type):
+                if crossref_mime_type(comp.mime_type):
                     format_tag = SubElement(component_tag, 'format')
-                    format_tag.set("mime_type", self.crossref_mime_type(comp.mime_type))
+                    format_tag.set("mime_type", crossref_mime_type(comp.mime_type))
 
             if comp.permissions:
                 self.set_component_permissions(component_tag, comp.permissions)
@@ -911,51 +912,6 @@ class CrossrefXML(object):
         tag_converted_string = eautils.replace_tags(tag_converted_string, 'bold', 'b')
         tag_converted_string = eautils.replace_tags(tag_converted_string, 'underline', 'u')
         return tag_converted_string
-
-    def crossref_mime_type(self, jats_mime_type):
-        """
-        Dictionary of lower case JATS mime type to crossRef schema mime type
-        """
-        mime_types = {}
-
-        mime_types['application/eps'] = 'application/eps'
-        mime_types['application/gz'] = 'application/gzip'
-        mime_types['application/tar.gz'] = 'application/gzip'
-        mime_types['application/doc'] = 'application/msword'
-        mime_types['application/pdf'] = 'application/pdf'
-        mime_types['application/rtf'] = 'application/rtf'
-        mime_types['application/xls'] = 'application/vnd.ms-excel'
-        mime_types['application/pptx'] = (
-            'application/vnd.openxmlformats-officedocument.presentationml.presentation')
-        mime_types['application/xlsx'] = (
-            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        mime_types['application/docx'] = (
-            'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-        mime_types['application/xml'] = 'application/xml'
-
-        mime_types['chemical/pdb'] = 'chemical/x-pdb'
-
-        mime_types['image/tif'] = 'image/tiff'
-        mime_types['image/tiff'] = 'image/tiff'
-        mime_types['application/png'] = 'image/png'
-
-        mime_types['text/plain'] = 'text/plain'
-        mime_types['application/csv'] = 'text/plain'
-        mime_types['application/txt'] = 'text/plain'
-        mime_types['text/pl'] = 'text/plain'
-        mime_types['text/py'] = 'text/plain'
-        mime_types['text/txt'] = 'text/plain'
-        mime_types['text/rtf'] = 'text/rtf'
-
-        mime_types['video/avi'] = 'video/avi'
-        mime_types['video/mp4'] = 'video/mp4'
-        mime_types['video/mpeg'] = 'video/mpeg'
-        mime_types['video/mpg'] = 'video/mpeg'
-        mime_types['video/mov'] = 'video/quicktime'
-        mime_types['video/wmv'] = 'video/x-ms-wmv'
-        mime_types['video/gif'] = 'image/gif'
-
-        return mime_types.get(jats_mime_type.lower())
 
     def output_xml(self, pretty=False, indent=""):
         encoding = 'utf-8'
