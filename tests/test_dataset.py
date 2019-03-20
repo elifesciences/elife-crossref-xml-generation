d9ea188e0e6ae8735b2ec136b8a6595a6986c374
Graham Nott <gnott@starglobal.ca>
2019-03-19 17:54:24 -0700

    Test choose_dataset_identifier for coverage.

---
+++ tests/test_dataset.py
@@ -0,0 +1,13 @@
+import unittest
+from elifecrossref import dataset
+
+
+class TestDataset(unittest.TestCase):
+
+    def test_choose_dataset_identifier_none(self):
+        """test when an object has no attributes"""
+        self.assertIsNone(dataset.choose_dataset_identifier(None))
+
+
+if __name__ == '__main__':
+    unittest.main()
