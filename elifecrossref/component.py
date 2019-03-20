aec2ea48a8f19b8bf054631116b1ace22ac21c7f
Graham Nott <gnott@starglobal.ca>
2019-03-20 15:53:42 -0700

    Refactor component tag to multiple functions.

--- elifecrossref/component.py
+++ elifecrossref/component.py
@@ -11,54 +11,70 @@ def set_component_list(parent, poa_article, crossref_config):
 
     component_list_tag = SubElement(parent, 'component_list')
     for comp in poa_article.component_list:
-        component_tag = SubElement(component_list_tag, 'component')
-        component_tag.set("parent_relation", "isPartOf")
-
-        titles_tag = SubElement(component_tag, 'titles')
-
-        title_tag = SubElement(titles_tag, 'title')
-        title_tag.text = comp.title
-
-        if comp.subtitle:
-            set_subtitle(titles_tag, comp, crossref_config.get('face_markup'))
-
-        if comp.mime_type:
-            # Convert to allowed mime types for Crossref, if found
-            if mime_type.crossref_mime_type(comp.mime_type):
-                format_tag = SubElement(component_tag, 'format')
-                format_tag.set("mime_type", mime_type.crossref_mime_type(comp.mime_type))
-
-        if comp.permissions:
-            set_component_permissions(component_tag, comp.permissions,
-                                      crossref_config.get('component_license_ref'))
-
-        if comp.doi:
-            # Try generating a resource value then continue
-            resource = resource_url.generate_resource_url(
-                comp, poa_article, crossref_config)
-            if resource and resource != '':
-                doi_data_tag = SubElement(component_tag, 'doi_data')
-                doi_tag_tag = SubElement(doi_data_tag, 'doi')
-                doi_tag_tag.text = comp.doi
-                resource_tag = SubElement(doi_data_tag, 'resource')
-                resource_tag.text = resource
+        set_component(component_list_tag, poa_article, comp, crossref_config)
 
 
-def set_component_permissions(parent, permissions, license_href):
+def set_component(parent, poa_article, comp, crossref_config):
+    component_tag = SubElement(parent, 'component')
+    component_tag.set("parent_relation", "isPartOf")
+
+    set_component_titles(component_tag, comp, crossref_config)
+    set_component_mime_type(component_tag, comp)
+    set_component_permissions(component_tag, comp, crossref_config)
+    set_component_doi(component_tag, poa_article, comp, crossref_config)
+
+
+def set_component_titles(parent, comp, crossref_config):
+    titles_tag = SubElement(parent, 'titles')
+    title_tag = SubElement(titles_tag, 'title')
+    title_tag.text = comp.title
+    if comp.subtitle:
+        set_subtitle(titles_tag, comp, crossref_config.get('face_markup'))
+
+
+def set_component_mime_type(parent, comp):
+    if comp.mime_type:
+        # Convert to allowed mime types for Crossref, if found
+        if mime_type.crossref_mime_type(comp.mime_type):
+            format_tag = SubElement(parent, 'format')
+            format_tag.set("mime_type", mime_type.crossref_mime_type(comp.mime_type))
+
+
+def set_component_permissions(parent, comp, crossref_config):
     """Specific license for the component"""
-    # First check if a license ref is in the config
-    if not license_href:
+    license_href = crossref_config.get('component_license_ref')
+    # First check if a license should be added
+    if not license_href or not comp.permissions:
         return
-    # set the component permissions if it has any copyright statement or license value
-    set_permissions = False
-    for permission in permissions:
-        if permission.get('copyright_statement') or permission.get('license'):
-            set_permissions = True
-    if set_permissions:
+    if do_set_component_permissions(comp):
         component_ai_program_tag = SubElement(parent, 'ai:program')
         component_ai_program_tag.set('name', 'AccessIndicators')
         license_ref_tag = SubElement(component_ai_program_tag, 'ai:license_ref')
         license_ref_tag.text = license_href
+
+
+def do_set_component_permissions(comp):
+    """decide whether to set a component permissions"""
+    if not comp.permissions:
+        return
+    for permission in comp.permissions:
+        # set the component permissions if it has any copyright statement or license value
+        if permission.get('copyright_statement') or permission.get('license'):
+            return True
+    return False
+
+
+def set_component_doi(parent, poa_article, comp, crossref_config):
+    if not comp.doi:
+        return
+    # Try generating a resource value then continue
+    resource = resource_url.generate_resource_url(comp, poa_article, crossref_config)
+    if resource:
+        doi_data_tag = SubElement(parent, 'doi_data')
+        doi_tag_tag = SubElement(doi_data_tag, 'doi')
+        doi_tag_tag.text = comp.doi
+        resource_tag = SubElement(doi_data_tag, 'resource')
+        resource_tag.text = resource
 
 
 def set_subtitle(parent, component, face_markup=None):
---
+++ tests/test_component.py
@@ -0,0 +1,16 @@
+import unittest
+from elifearticle.article import Component
+from elifecrossref import component
+
+
+class TestComponent(unittest.TestCase):
+
+    def test_do_set_component_permissions_none(self):
+        comp = Component()
+        self.assertIsNone(component.do_set_component_permissions(comp))
+
+    def test_do_set_component_permissions_false(self):
+        comp = Component()
+        permission = {}
+        comp.permissions = [permission]
+        self.assertFalse(component.do_set_component_permissions(comp))
