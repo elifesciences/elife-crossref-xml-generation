45fc5ad53a154b91fb45e7e0669b9c28527fd5c5
Graham Nott <gnott@starglobal.ca>
2019-04-04 10:55:45 -0700

    Extra argument was removed lately for peer_review.

--- elifecrossref/body.py
+++ elifecrossref/body.py
@@ -10,4 +10,4 @@ def set_body(parent, poa_articles, crossref_config, default_pub_date, submission
             # Create a new journal record for each article
             journal.set_journal(body_tag, poa_article, crossref_config, default_pub_date)
         elif submission_type == 'peer_review':
-            peer_review.set_peer_review(body_tag, poa_article, crossref_config, default_pub_date)
+            peer_review.set_peer_review(body_tag, poa_article, crossref_config)
