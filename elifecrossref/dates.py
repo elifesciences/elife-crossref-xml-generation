b4d586d0d9b8738fde2b09a1cd87a17ee40c4681
Graham Nott <gnott@starglobal.ca>
2019-04-03 17:07:00 -0700

    Refactor setting date month, day and year tags.

--- elifecrossref/dates.py
+++ elifecrossref/dates.py
@@ -6,9 +6,13 @@ def set_publication_date(parent, pub_date):
     if pub_date:
         publication_date_tag = SubElement(parent, 'publication_date')
         publication_date_tag.set("media_type", "online")
-        month_tag = SubElement(publication_date_tag, "month")
-        month_tag.text = str(pub_date.tm_mon).zfill(2)
-        day_tag = SubElement(publication_date_tag, "day")
-        day_tag.text = str(pub_date.tm_mday).zfill(2)
-        year_tag = SubElement(publication_date_tag, "year")
-        year_tag.text = str(pub_date.tm_year)
+        set_date_detail(publication_date_tag, pub_date)
+
+
+def set_date_detail(parent, pub_date):
+    month_tag = SubElement(parent, "month")
+    month_tag.text = str(pub_date.tm_mon).zfill(2)
+    day_tag = SubElement(parent, "day")
+    day_tag.text = str(pub_date.tm_mday).zfill(2)
+    year_tag = SubElement(parent, "year")
+    year_tag.text = str(pub_date.tm_year)
