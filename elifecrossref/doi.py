3f6f93056474152e819ab455754444250bf38db1
Graham Nott <gnott@starglobal.ca>
2019-08-08 15:54:59 -0700

    When etting doi resource, pass in obj and parent.

--- elifecrossref/doi.py
+++ elifecrossref/doi.py
@@ -3,11 +3,11 @@ from elifecrossref import collection, resource_url
 
 
 def set_article_doi_data(parent, poa_article, crossref_config):
-    doi_data_tag = set_doi_data(parent, poa_article, crossref_config)
+    doi_data_tag = set_doi_data(parent, poa_article, poa_article, crossref_config)
     collection.set_collection(doi_data_tag, poa_article, "text-mining", crossref_config)
 
 
-def set_doi_data(parent, poa_article, crossref_config):
+def set_doi_data(parent, obj, poa_article, crossref_config, pattern_type=None):
     doi_data_tag = SubElement(parent, 'doi_data')
 
     doi_tag = SubElement(doi_data_tag, 'doi')
@@ -16,7 +16,7 @@ def set_doi_data(parent, poa_article, crossref_config):
     resource_tag = SubElement(doi_data_tag, 'resource')
 
     resource = resource_url.generate_resource_url(
-        poa_article, poa_article, crossref_config)
+        obj, poa_article, crossref_config, pattern_type)
     resource_tag.text = resource
 
     return doi_data_tag
