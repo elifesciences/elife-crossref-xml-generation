7f3afa9fec21e63ba618785908edfac3eed02a1c
Graham Nott <gnott@starglobal.ca>
2021-10-19 09:15:20 -0700

    Add title to pending_publication deposit.

--- elifecrossref/pending_publication.py
+++ elifecrossref/pending_publication.py
@@ -1,5 +1,5 @@
 from xml.etree.ElementTree import SubElement
-from elifecrossref import dates, journal
+from elifecrossref import dates, journal, title
 
 
 def set_pending_publication(parent, poa_article, crossref_config):
@@ -10,6 +10,8 @@ def set_pending_publication(parent, poa_article, crossref_config):
     publication_full_title_tag.text = crossref_config.get("registrant")
 
     journal.set_issn_tag(publication_tag, poa_article)
+
+    title.set_titles(pending_publication_tag, poa_article.title, crossref_config)
 
     set_acceptance_date(pending_publication_tag, poa_article.get_date("accepted"))
 
